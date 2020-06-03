0c0453a437e338b87e37d146e1ed6caa8412f46e
Netranand <netranandm3@gmail.com>
2020-06-03 12:38:53 +0530

    make it better

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ app.use(logger('dev'));
 app.use('/users', users);
 app.use('/newusers', newusers);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '..', 'client', 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '..', 'client', 'build', 'index.html')
-    );
-  });
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'))
+  const path = require('path');
+  app.get('*',(req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
 }
 
 app.listen(port, () => {
